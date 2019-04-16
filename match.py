b0561dd173d491e4f958ca153e53d6a4faf4d959
yangjc <bupt_yjc@163.com>
2019-04-16 20:30:34 +0800

    change to clean

--- match.py
+++ match.py
@@ -1,7 +1,5 @@
 import pickle as pk
 
-import re
-
 import numpy as np
 
 from collections import Counter
@@ -11,11 +9,13 @@ from keras.layers import Input
 
 from keras.preprocessing.sequence import pad_sequences
 
+from preprocess import clean
+
 from nn_arch import match
 
 from encode import load_encode
 
-from util import load_word_re, load_type_re, load_pair, word_replace, map_item
+from util import map_item
 
 
 def define_match(encode_len):
@@ -39,15 +39,6 @@ def load_cache(path_cache):
 
 seq_len = 30
 encode_len = 200
-
-path_stop_word = 'dict/stop_word.txt'
-path_type_dir = 'dict/word_type'
-path_homo = 'dict/homo.csv'
-path_syno = 'dict/syno.csv'
-stop_word_re = load_word_re(path_stop_word)
-word_type_re = load_type_re(path_type_dir)
-homo_dict = load_pair(path_homo)
-syno_dict = load_pair(path_syno)
 
 path_embed = 'feat/embed.pkl'
 path_word2ind = 'model/word2ind.pkl'
@@ -76,11 +67,7 @@ models = {'dnn_encode': load_encode('dnn', embed_mat, seq_len),
 
 
 def predict(text, name, vote):
-    text = re.sub(stop_word_re, '', text.strip())
-    for word_type, word_re in word_type_re.items():
-        text = re.sub(word_re, word_type, text)
-    text = word_replace(text, homo_dict)
-    text = word_replace(text, syno_dict)
+    text = clean(text)
     core_sents, core_labels = map_item(name, caches)
     seq = word2ind.texts_to_sequences([text])[0]
     pad_seq = pad_sequences([seq], maxlen=seq_len)
